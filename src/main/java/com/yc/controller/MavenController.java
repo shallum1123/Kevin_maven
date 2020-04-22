3faf127a7a88bae270f47015f26224c8096e6b49
git <Shallum@yc_No1>
2020-04-22 16:15:23 +0800

    ecplise测试提交项目到私有仓库0.0.1

---
+++ src/main/java/com/yc/controller/MavenController.java
@@ -0,0 +1,13 @@
+package com.yc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MavenController {
+	@RequestMapping("test")
+	public String testMaven() {
+		return "SUCCESS----QQQQQQQQQ---ceshicssdeishiceshi";
+	}
+}
